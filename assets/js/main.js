3afd258096be7dbb29bf29976fd45ae6c039f244
LucasMRomao <lmartins.romao@gmail.com>
2022-07-04 19:40:45 -0300

    Commit teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,21 @@ $(function(){
     $("#principal").load("home.html");
 
     $("#lHome").click(function(){
+        console.log("Load Home");
         $("#principal").load("home.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niHome").addClass("active");
     })
 
     $("#lCalendario").click(function(){
+        console.log("Load Calendário");
         $("#principal").load("calendario.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niCalendario").addClass("active");
     });
 
     $("#lPedido").click(function(){
+        console.log("Load Pedido Oração");
         $("#principal").load("pedido_oracao.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niContato").addClass("active");
