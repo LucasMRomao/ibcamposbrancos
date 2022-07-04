188dbfc8ad00734b5c822fd89c84363db374fe67
LucasMRomao <lmartins.romao@gmail.com>
2022-07-04 19:23:15 -0300

    Correção links oficial

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,19 @@ $(function(){
     $("#principal").load("home.html");
 
     $("#lHome").click(function(){
-        $("#principal").load("../../home.html");
+        $("#principal").load("home.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niHome").addClass("active");
     })
 
     $("#lCalendario").click(function(){
-        $("#principal").load("../../calendario.html");
+        $("#principal").load("calendario.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niCalendario").addClass("active");
     });
 
     $("#lContato").click(function(){
-        $("#principal").load("../../pedido_oracao.html");
+        $("#principal").load("pedido_oracao.html");
         $(".nbMenu>.active").removeClass("active");
         $(".niContato").addClass("active");
     });
