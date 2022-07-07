96504306b99d2df2d4662915ecf289874620566e
LucasMRomao <lmartins.romao@gmail.com>
2022-07-06 21:14:30 -0300

    Correção item ativo menu superior

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,18 @@ $(function(){
     let loadPage = function (pagina) {
         $("#principal").load(pagina);
         $(".nbMenu>.active").removeClass("active");
-        $(".niContato").addClass("active");
+        //$(".niContato").addClass("active");
+
+        switch(pagina){
+            case 'home.html': $(".niHome").addClass("active"); break;
+            case 'calendario.html': $(".niCalendario").addClass("active"); break;
+            case 'pedido_oracao.html': $(".niPedido").addClass("active"); break;
+        }
     }
 
     $("#principal").load("home.html");
     $("#lHome").click(() => loadPage("home.html"));
     $("#lCalendario").click(() => loadPage("calendario.html"));
     $("#lPedido").click(() => loadPage("pedido_oracao.html"));
-
     
 });
