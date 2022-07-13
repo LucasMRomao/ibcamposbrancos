15eb374a3f08dcefeab174406a31adab36a74025
LucasMRomao <lmartins.romao@gmail.com>
2022-07-13 20:48:48 -0300

    Retirado console.log do id do pedido

--- pastor/main.js
+++ pastor/main.js
@@ -43,7 +43,6 @@ $(function(){
                         $("#dPedidos").html(""); //Limpa pedidos
                         
                         dataPedido.docs.map((val) => {
-                            console.log("ID: " + val.id);
                             let nome = val.data().nome;
                             let whatsapp = val.data().whatsapp;
                             let descricao = val.data().descricao_pedido;
