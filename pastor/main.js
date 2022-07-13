91161e6244502094dd7abd59f717fb8a90a45bae
LucasMRomao <lmartins.romao@gmail.com>
2022-07-13 20:32:51 -0300

    Correção src img pedido lido

--- pastor/main.js
+++ pastor/main.js
@@ -55,7 +55,7 @@ $(function(){
                             publico ? $pedido += "<div class='alert alert-success' role='alert' style='margin-top: 10px;'>" : $pedido += "<div class='alert alert-warning' role='alert' style='margin-top: 10px;'>";
                             $pedido += "<div class='row'>";
                             $pedido += "<div class='col-1'>";
-                            lido ? $pedido += "<img class='img-fluid' src='../assets/img/'lido.jpg />" : $pedido += "<a href='#' class='lPedidoLido' pedido-id='" + val.id + "'><img class='img-fluid' src='../assets/img/nao-lido.jpg' /></a>";
+                            lido ? $pedido += "<img class='img-fluid' src='../assets/img/lido.jpg' />" : $pedido += "<a href='#' class='lPedidoLido' pedido-id='" + val.id + "'><img class='img-fluid' src='../assets/img/nao-lido.jpg' /></a>";
                             $pedido += "</div>"; //Close div col-1
                             $pedido += "<div class='col-11'>"
                             $pedido += "<b>" + nome + "</b><br/>";
