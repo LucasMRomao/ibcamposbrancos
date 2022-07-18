45eb258b062e0473d9323686b34be7bc9b202929
LucasMRomao <lmartins.romao@gmail.com>
2022-07-18 18:19:01 -0300

    Adicionada função para carregar versiculo semanal da base

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,5 +11,38 @@ $(function(){
     $("#lCultos").click(() => loadPage("cultos.html", "Cultos"));
     $("#lPedido").click(() => loadPage("pedido_oracao.html", "Pedido"));
     $("#lTestemunho").click(() => loadPage("testemunho.html", "Testemunho"));
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyC_Lz-HIXHPz90MM_8vERto3dUK1rGts44",
+        authDomain: "igreja-batista-campos-brancos.firebaseapp.com",
+        databaseURL: "https://igreja-batista-campos-brancos-default-rtdb.firebaseio.com",
+        projectId: "igreja-batista-campos-brancos",
+        storageBucket: "igreja-batista-campos-brancos.appspot.com",
+        messagingSenderId: "724470922283",
+        appId: "1:724470922283:web:fcf1eaf1f43df1d48dfe6e",
+        measurementId: "G-WKZFS0PDDY"
+    };
+
+    if(!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const auth = firebase.auth();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const db = firebase.firestore();
+
+    db.collection("versiculo_semanal").doc("tXDk9jv0SOvX4MF5Muho").onSnapshot((data) => {
+        data.docs.map((val) => { //Similar ao foreach do PHP
+            let livro = val.data().livro;
+            let capitulo = val.data().capitulo;
+            let versiculo = val.data().versiculo;
+            let descricao_versiculo = val.data().descricao_versiculo;
+
+            $("#sLivro").text(livro);
+            $("#sCapitulo").text(capitulo);
+            $("#sVersiculo").text(versiculo);
+            $("#sDescricaoVersiculo").text(descricao_versiculo);
+        });
+    });
     
 });
