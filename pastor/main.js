011d024bf4544590c8cdc2d5d145230431d62973
LucasMRomao <lmartins.romao@gmail.com>
2022-07-29 19:04:23 -0300

    Teste await envio de fotos células

--- pastor/main.js
+++ pastor/main.js
@@ -187,7 +187,7 @@ $(function(){
                 for(var i in fotos){
                     const uploadTask = storage.ref(`celulas/arquivo-${contFotos}`).put(fotos[i]);
 
-                    uploadTask.on('state_change', (snapshot) => {
+                    await uploadTask.on('state_change', (snapshot) => {
                         /*let progress = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
                         $("#pEnvioArquivo").val(progress);
                         $("#sPorcentagemEnvio").text(Math.round(progress) + "%");*/
